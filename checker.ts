05e92eae8b75a5204729152d4f4d38af80fb9e5c
aariprom <jseokp4@gmail.com>
2025-01-04 18:33:34 +0900

    problem 1-4 fix

--- checker.ts
+++ checker.ts
@@ -10,22 +10,19 @@ const prisma = new PrismaClient()
 
 function problem1() {
   return prisma.$queryRaw`
-    select firstName, lastName, income from Customer 
+    select firstName, lastName, income 
+    from Customer 
     where income between 50000 and 60000 
     order by income desc, lastName asc, firstName asc 
     LIMIT 10;`
 }
 
-/*
-  왜 CAST((m.salary - e.salary) as CHAR(10))으로 하지 않으면 Incorrect인가요?
-*/
-
 function problem2() {
   return prisma.$queryRaw`
     select e.sin, b.branchName, e.salary, CAST((m.salary - e.salary) AS char(10)) as 'Salary Diff'
     from Employee e
-    join Branch b on e.branchNumber = b.branchNumber
-    join Employee m on b.managerSIN = m.sin
+      left join Branch b on e.branchNumber = b.branchNumber
+      left join Employee m on b.managerSIN = m.sin
     where b.branchName in ('London', 'Berlin')
     order by (m.salary - e.salary) desc
     LIMIT 10;
@@ -43,7 +40,7 @@ function problem3() {
       )
     order by lastName asc, firstName asc
     LIMIT 10;
-    `
+  `
 }
 
 function problem4() {
@@ -51,23 +48,17 @@ function problem4() {
     with customer_owns_account_with_branch as (
       select o.customerID, c.income, o.accNumber, a.branchNumber, b.branchName
       from Owns o
-             left join Customer c on o.customerID = c.customerID
-             left join Account a on o.accNumber = a.accNumber
-             left join Branch b on a.branchNumber = b.branchNumber
+        left join Customer c on o.customerID = c.customerID
+        left join Account a on o.accNumber = a.accNumber
+        left join Branch b on a.branchNumber = b.branchNumber
       where c.income > 80000
-    ), customer_owns_account_at_london as (
-      select customerID from customer_owns_account_with_branch
-      where branchName = 'London'
-    ), customer_owns_account_at_latveria as (
-      select customerID from customer_owns_account_with_branch
-      where branchName = 'Latveria'
     ) select customerID, income, accNumber, branchNumber
     from customer_owns_account_with_branch
-    where customerID in (select customerID from customer_owns_account_at_london)
-      and customerID in (select customerID from customer_owns_account_at_latveria)
+    where customerID in (select c1.customerID from customer_owns_account_with_branch c1 where c1.branchName = 'London')
+      and customerID in (select c2.customerID from customer_owns_account_with_branch c2 where c2.branchName = 'Latveria')
     order by customerID asc, accNumber asc
     LIMIT 10;
-`
+  `
 }
 
 function problem5() {
