549e4a975a671f575d9525fa4481d82d494c1a8b
aariprom <jseokp4@gmail.com>
2025-01-14 02:40:38 +0900

    prisma /account/account_no/withdraw

--- src/routes/accountRoutes.ts
+++ src/routes/accountRoutes.ts
@@ -16,3 +16,15 @@ accountRouter.post('/account/:account_no/deposit', async (req, res) => {
         res.status(500).send({ message: e.message });
     }
 });
+
+accountRouter.post('/account/:account_no/withdraw', async (req, res) => {
+    try {
+        const { accNumber, customerID, amount } = req.body;
+        const updatedBalance =
+            await accountService.withdraw({ accNumber, customerID, amount });
+        res.send(updatedBalance);
+    } catch (e: any) {
+        console.error(e);
+        res.status(500).send({ message: e.message });
+    }
+});
--- src/services/accountService.ts
+++ src/services/accountService.ts
@@ -1,7 +1,7 @@
 import prisma from '../prismaClient';
 
 export class AccountService {
-    async deposit({ accNumber, customerID, amount, }: {
+    async deposit ({ accNumber, customerID, amount, }: {
         accNumber: number, customerID: number, amount: number,
     }) {
         return prisma.$transaction(async (prisma) => {
@@ -47,4 +47,55 @@ export class AccountService {
             return updatedBalance;
         });
     }
+
+    async withdraw ({ accNumber, customerID, amount, }: {
+        accNumber: number, customerID: number, amount: number,
+    }) {
+        return prisma.$transaction(async (prisma) => {
+            const account = await prisma.owns.findUnique({
+                select: {
+                    Account: {
+                        select: {
+                            balance: true,
+                        },
+                    },
+                },
+                where: {
+                    customerID_accNumber: {
+                        customerID,
+                        accNumber,
+                    },
+                },
+            });
+
+            const currentBalance = Number(account?.Account.balance);
+
+            if (!account) {
+                throw new Error('Forbidden');
+            } else if (currentBalance < amount) {
+                throw new Error('Insufficient balance');
+            }
+
+            const updatedBalance = (await prisma.account.update({
+                where: { accNumber },
+                data: { balance: String(currentBalance - amount) }
+            })).balance;
+
+            const nextTransNumber = ((await prisma.transactions.findFirst({
+                where: { accNumber },
+                orderBy: { transNumber: 'desc' },
+                select: { transNumber: true },
+            }))?.transNumber || 0) + 1;
+
+            const transactionLog = await prisma.transactions.create({
+                data: {
+                    accNumber,
+                    transNumber: nextTransNumber,
+                    amount: '-'+String(amount),
+                },
+            });
+
+            return updatedBalance;
+        });
+    };
 }
